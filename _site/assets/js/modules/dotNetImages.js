839927c04d14e15806e2ccbaba4d1b104f2185c0
BP <14paxton@gmail.com>
2024-11-18 12:49:03 -0600

    hibernate clean

--- _site/assets/js/modules/dotNetImages.js
+++
@@ -1,8 +0,0 @@
-import {dotNetImages} from "/assets/js/Objects/DotNetImages.js";
-import {addCSSImageContainers} from "/assets/js/modules/createCSSImageContainers.js";
-
-export default async function () {
-    console.log("create images dotNetImages");
-    console.log("images added dotNetImages");
-    await addCSSImageContainers(dotNetImages, "dotNetImages.js");
-}
